fe50230ca1d33788a82fcd0532005e2ae14ab343
majesticpurr <majesticpurr@jg.drl.pl>
2018-10-26 00:18:37 +0200

    Data storage object passed to UserService object's constructor

--- src/main/java/pl/kurs/jg/tdd/userservice/UserService.java
+++ src/main/java/pl/kurs/jg/tdd/userservice/UserService.java
@@ -1,4 +1,14 @@
 package pl.kurs.jg.tdd.userservice;
 
 public class UserService {
+    private final UserStorage userStorage;
+
+    public UserService(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+    public User find(String login) {
+        return null;
+    }
+
 }
---
+++ src/main/java/pl/kurs/jg/tdd/userservice/UserStorage.java
@@ -0,0 +1,9 @@
+package pl.kurs.jg.tdd.userservice;
+
+public class UserStorage {
+
+    public User read(String login) {
+        return null;
+    }
+
+}
