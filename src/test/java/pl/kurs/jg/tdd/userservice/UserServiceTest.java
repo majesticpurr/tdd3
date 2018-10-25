690ee6109164eb989050a15e695eead86504f556
majesticpurr <majesticpurr@jg.drl.pl>
2018-10-26 00:22:21 +0200

    Test for finding a user provided by data storage

--- src/test/java/pl/kurs/jg/tdd/userservice/UserServiceTest.java
+++ src/test/java/pl/kurs/jg/tdd/userservice/UserServiceTest.java
@@ -1,4 +1,34 @@
 package pl.kurs.jg.tdd.userservice;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
 public class UserServiceTest {
+
+    @Test
+    public void shouldFindUser() {
+
+        //given
+
+        String login = "jdoe";
+        String name = "John";
+        String lastName = "Doe";
+
+        User user = new User(login, name, lastName);
+
+        UserStorage userStorage = Mockito.mock(UserStorage.class);
+        BDDMockito.given(userStorage.read(ArgumentMatchers.eq(login))).willReturn(user);
+
+        UserService userService = new UserService(userStorage);
+
+        // when
+        User result = userService.find(login);
+
+        // then
+        Assert.assertEquals(user, result);
+
+    }
 }
